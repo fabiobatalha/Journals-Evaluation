fd4f96fa1197e7011717e8020adb2fc4e39a2cea
Fabio Batalha <fabiobcs@yahoo.com.br>
2010-11-18 18:25:45 -0200

    Removing pyc file to put them on the .gitignore file and django-env

--- django-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/environment/Applications/avaliacao/django-env/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.2.3','django-admin.py'
-__requires__ = 'Django==1.2.3'
-import pkg_resources
-pkg_resources.run_script('Django==1.2.3', 'django-admin.py')
